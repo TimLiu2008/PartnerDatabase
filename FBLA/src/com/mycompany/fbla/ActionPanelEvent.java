192a76ab2d10ee4356e1fec6053b75a9d4d99fd5
TimLiu2008 <132114460+TimLiu2008@users.noreply.github.com>
2024-02-25 02:52:22 -0500

    Add files via upload

---
+++ FBLA/src/com/mycompany/fbla/ActionPanelEvent.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.fbla;
+
+/**
+ *
+ * @author Tim
+ */
+public interface ActionPanelEvent {
+    //Removes the row when the remove button is pressed
+    public void onRemove(int row);
+    
+}
